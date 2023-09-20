8feff8b0c47e68f6bf2548f68f2d53f95e506f64
Arina <arina-pivovar@mail.ru>
2023-09-20 12:33:39 +0300

    Сделала задачи 1-3, 5-6

--- 6.py
+++
@@ -1,5 +0,0 @@
-my_tuple = tuple(map(int, input("Введите элементы кортежа через пробел: ").split()))
-for i in range(len(my_tuple)):
-    if my_tuple[i] < 0:
-        print("Индекс первого отрицательного элемента:", i)
-        break
